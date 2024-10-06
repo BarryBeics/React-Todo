b46f51a2e6bcdd0e10626606d224d16ee55b2577
barry Marples <57953984+BarryBeics@users.noreply.github.com>
2024-10-06 21:24:37 +0100

    define the data

---
+++ todo-list-app/src/data/sampleData.ts
@@ -0,0 +1,33 @@
+// A few sample tasks to work with
+export const sampleTasks = [
+    {
+        id: '1',
+        title: 'Wash the dog',
+        description: 'Use lots of soap & treats',
+        completed: false,
+    },
+    {
+        id: '2',
+        title: 'Tax the car',
+        description: 'Get MOT and insurance docs',
+        completed: true,
+    },
+    {
+        id: '3',
+        title: 'Book a holiday',
+        description: 'Check passports',
+        completed: false,
+    },
+    {
+        id: '4',
+        title: 'Cut the grass',
+        description: '',
+        completed: false,
+    },
+    {
+        id: '5',
+        title: 'Get a new Job',
+        description: 'Prepare you CV',
+        completed: true,
+    }
+]
