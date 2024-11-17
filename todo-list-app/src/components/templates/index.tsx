15eced5efb291f3062ce80b2917fec597cb45983
barry Marples <57953984+BarryBeics@users.noreply.github.com>
2024-11-17 16:38:34 +0000

    Update from toDO to item list

--- todo-list-app/src/components/templates/index.tsx
+++ todo-list-app/src/components/templates/index.tsx
@@ -1,3 +1,3 @@
 export { default as FavouriteList } from './FavouriteList/FavouriteList';
 export { default as InputForm } from './InputForm/InputForm';
-export { default as TodoList } from './TodoList/TodoList';
+export { default as TodoList } from './ItemList/ItemList';
